2c1e82ce5a64268f7adffde9d62d1d257b93b379
Kevin Buhler <kevinbuhlerr@gmail.com>
2022-06-19 18:03:57 -0600

    update profile and change password added to backend

--- server/users/views.py
+++ server/users/views.py
@@ -2,8 +2,8 @@
 # Create your views here.
 # from .models import CustomUser
 from rest_framework import viewsets, permissions
-from .serializers import UserSerializer, MyTokenObtainPairSerializer
-from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer, MyTokenObtainPairSerializer, ChangePasswordSerializer, UpdateUserSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import RegisterSerializer
 from rest_framework import generics
@@ -26,3 +26,16 @@ class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+class ChangePasswordView(generics.UpdateAPIView):
+
+    queryset = CustomUser.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
+
+
+class UpdateProfileView(generics.UpdateAPIView):
+
+    queryset = CustomUser.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UpdateUserSerializer
